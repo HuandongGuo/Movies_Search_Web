55ac92f8154b122a527e36d6953d257f9c5b0a50
huandong <huandong@buffalo.edu>
2025-02-02 21:44:43 -0500

    fixed api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 //  1acbf7f4
 
-const API_URL = "http://www.omdbapi.com?apikey=1acbf7f4";
+const API_URL = "https://www.omdbapi.com?apikey=1acbf7f4";
 
 const App = () => {
     const[movies, setMovies] = useState([]);
